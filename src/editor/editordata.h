cb49b8b82fadac405574deeabcf01b807fa7cdc0
Jussi Lind <jussi.lind@iki.fi>
2011-10-09 21:45:58 +0300

    New: Adds comments to editordata.h

--- src/editor/editordata.h
+++ src/editor/editordata.h
@@ -30,24 +30,65 @@ class EditorData
 {
 public:
 
+    //! Editing modes.
     enum EditorMode {EM_NONE = 0, EM_SETROUTE, EM_SETTILETYPE};
 
+    //! Constructor.
+    //! \param mainWindow Pointer to the main window.
     EditorData(MainWindow * mainWindow);
+
+    //! Load track from the given file.
     bool loadTrackData(QString fileName);
+
+    //! Save track data.
     bool saveTrackData();
+
+    //! Sava track data to the given file.
     bool saveTrackDataAs(QString fileName);
+
+    //! Set track data as the given data.
     void setTrackData(TrackData * newTrackData);
+
+    //! Check if it's possible to set route.
     bool canRouteBeSet() const;
+
+    //! This should be called before user starts
+    //! setting the route.
     void beginSetRoute();
+
+    //! This should be called after user ends
+    //! setting the route.
     void endSetRoute();
+
+    //! Adds line objects to the scenes visualizing
+    //! the current route.
+    //! \param closeLoop The route will be considered a
+    //! closed loop if true.
     void addRouteLinesToScene(bool closeLoop);
+
+    //! Removes line objects from the scene.
     void removeRouteLinesFromScene();
+
+    //! Returns current track data object.
     TrackData * trackData();
+
+    //! Returns current editing mode.
     EditorMode mode() const;
+
+    //! Sets the current editing mode.
     void setMode(EditorMode newMode);
+
+    //! Returns the tile being drag'n'dropped
+    //! or NULL.
     TrackTile * dragAndDropSourceTile() const;
+
+    //! Set tile that is being drag'n'dropped.
     void setDragAndDropSourceTile(TrackTile * tile);
+
+    //! Get source pos of the drag'n'drop.
     QPointF dragAndDropSourcePos() const;
+
+    //! Set source pos of the drag'n'drop.
     void setDragAndDropSourcePos(QPointF pos);
 
 private:
