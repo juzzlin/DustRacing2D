d29728d18526ebce7f9408df09b8cf5f84130cfc
Jussi Lind <jussi.lind@iki.fi>
2012-01-24 00:41:54 +0200

    Shadow support for MCTextureText.

--- src/game/MiniCore/Text/mctexturetext.cc
+++ src/game/MiniCore/Text/mctexturetext.cc
@@ -165,10 +165,78 @@ void MCTextureText::render(MCFloat x, MCFloat y, MCCamera * pCamera,
 void MCTextureText::renderShadow(MCFloat x, MCFloat y, MCCamera * pCamera,
     MCTextureFont & font, bool newLineIncreasesY) const
 {
+    // TODO: Refactor and optimize this..
+
     if (pCamera)
     {
         pCamera->mapToCamera(x, y);
     }
 
-    // TODO
+    glPushAttrib(GL_ENABLE_BIT);
+
+    glDisable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+    glNormal3i(0, 0, 1);
+
+    font.surface().doAlphaTest();
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ZERO, GL_ZERO);
+
+    glNormal3f(0.0f, 0.0f, 1.0f);
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    glBindTexture(GL_TEXTURE_2D, font.surface().handle());
+
+    glPushMatrix();
+    glTranslated(x, y, 0);
+
+    const MCFloat w2 = m_glyphWidth / 2;
+    const MCFloat h2 = m_glyphHeight / 2;
+
+    MCFloat glyphXPos = 0;
+    MCFloat glyphYPos = 0;
+
+    glBegin(GL_QUADS);
+
+    const MCUint textSize = m_text.size();
+    for (MCUint i = 0; i < textSize; i++)
+    {
+        if (m_text.at(i) == '\n')
+        {
+            if (newLineIncreasesY)
+            {
+                glyphYPos += m_glyphHeight;
+            }
+            else
+            {
+                glyphYPos -= m_glyphHeight;
+            }
+
+            glyphXPos  = 0;
+        }
+        else if (m_text.at(i) == ' ')
+        {
+            glyphXPos += m_glyphWidth;
+        }
+        else
+        {
+            MCTextureGlyph & glyph = font.glyph(m_text.at(i));
+
+            glTexCoord2f(glyph.uv(0).m_u, glyph.uv(0).m_v);
+            glVertex3f(glyphXPos - w2, glyphYPos + h2, 0);
+            glTexCoord2f(glyph.uv(1).m_u, glyph.uv(1).m_v);
+            glVertex3f(glyphXPos + w2, glyphYPos + h2, 0);
+            glTexCoord2f(glyph.uv(2).m_u, glyph.uv(2).m_v);
+            glVertex3f(glyphXPos + w2, glyphYPos - h2, 0);
+            glTexCoord2f(glyph.uv(3).m_u, glyph.uv(3).m_v);
+            glVertex3f(glyphXPos - w2, glyphYPos - h2, 0);
+
+            glyphXPos += m_glyphWidth;
+        }
+    }
+
+    glEnd();
+
+    glPopMatrix();
+    glPopAttrib();
 }
