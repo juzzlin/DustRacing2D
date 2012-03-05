dde1559b7d8ebcd8052a914d9f6d6cf5e44c2c28
Jussi Lind <jussi.lind@iki.fi>
2012-03-05 21:34:10 +0200

    Initializes tests for MCObject.

---
+++ src/game/MiniCore/UnitTests/MCObjectTest/MCObjectTest.cpp
@@ -0,0 +1,44 @@
+// This file belongs to the "MiniCore" game engine.
+// Copyright (C) 2012 Jussi Lind <jussi.lind@iki.fi>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+// MA  02110-1301, USA.
+//
+
+#include "MCObjectTest.hpp"
+#include "../../Core/mcforcegenerator.hh"
+#include "../../Core/mcforceregistry.hh"
+#include "../../Core/mcobject.hh"
+#include "../../Core/mcworld.hh"
+
+MCObjectTest::MCObjectTest()
+{
+}
+
+void MCObjectTest::testMass()
+{
+    MCObject object("TestObject");
+    QVERIFY(qFuzzyCompare(object.mass(), 0.0f));
+
+    object.setMass(1000.0f);
+    QVERIFY(qFuzzyCompare(object.mass(), 1000.0f));
+    QVERIFY(qFuzzyCompare(object.invMass(), 1.0f / 1000.0f));
+
+    object.setMass(1, true);
+    QVERIFY(object.stationary());
+    QVERIFY(qFuzzyCompare(object.invMass(), 0.0f));
+}
+
+QTEST_MAIN(MCObjectTest)
