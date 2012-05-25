436d661ccdea104c079e47310eaa22235af9ffcd
Jussi Lind <jussi.lind@iki.fi>
2012-05-25 20:18:42 +0300

    Parameter tuning.

--- src/game/car.cpp
+++ src/game/car.cpp
@@ -38,9 +38,9 @@ namespace
     const MCFloat FRICTION             = 1.0f;
     const MCFloat BRAKING_FRICTION     = 1.0f;
     const MCFloat ROLLING_FRICTION     = 0.1f;
-    const MCFloat SLIDE_FRICTION       = 1.0f;
+    const MCFloat SLIDE_FRICTION       = 0.75f;
     const MCFloat ROTATION_FRICTION    = 1.0f;
-    const MCFloat OFF_TRACK_FRICTION   = 1.0f;
+    const MCFloat OFF_TRACK_FRICTION   = 0.75f;
     const MCFloat OFF_TRACK_MOMENT     = 50000.0f;
     const MCFloat TURNING_IMPULSE      = 0.25f;
     const MCFloat POWER                = 5000.0f;
