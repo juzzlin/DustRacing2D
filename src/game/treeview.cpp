3011569f4df1e3358068d5f3b4592067ff57caad
Jussi Lind <jussi.lind@iki.fi>
2012-06-24 20:45:02 +0300

    A bit taller trees.

--- src/game/treeview.cpp
+++ src/game/treeview.cpp
@@ -59,7 +59,7 @@ void TreeView::render(const MCVector3d<MCFloat> & l, MCFloat, MCCamera * pCamera
 
     surface()->enableClientState(true);
 
-    int z = branchHeight / 3;
+    int z = branchHeight;
     for (int i = 0; i < m_branches; i++)
     {
         MCFloat x = l.i() + m_top.i() * branchHeight / 4;
