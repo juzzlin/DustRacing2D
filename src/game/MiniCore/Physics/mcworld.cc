439ce8b0ef438844f490cf88ec1b72fdc5a03eb6
Jussi Lind <jussi.lind@iki.fi>
2012-08-06 21:17:01 +0300

    Don't reset motion when adding object to the world.

--- src/game/MiniCore/Physics/mcworld.cc
+++ src/game/MiniCore/Physics/mcworld.cc
@@ -448,7 +448,6 @@ void MCWorld::addObject(MCObject & object)
             // Add to object vector (O(1))
             objs.push_back(&object);
             object.setIndex(objs.size() - 1);
-            object.resetMotion();
 
             // Add to ObjectTree
             if (object.physicsObject() && !object.bypassCollisions())
