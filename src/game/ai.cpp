2a3728973fe38b266c1c891b68df87e0f671b629
Jussi Lind <jussi.lind@iki.fi>
2012-11-04 00:34:25 +0200

    Updates license texts with Dust Racing 2D.

--- src/game/ai.cpp
+++ src/game/ai.cpp
@@ -1,17 +1,17 @@
-// This file is part of Dust Racing (DustRAC).
+// This file is part of Dust Racing 2D.
 // Copyright (C) 2012 Jussi Lind <jussi.lind@iki.fi>
 //
-// DustRAC is free software: you can redistribute it and/or modify
+// Dust Racing 2D is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// DustRAC is distributed in the hope that it will be useful,
+// Dust Racing 2D is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with DustRAC. If not, see <http://www.gnu.org/licenses/>.
+// along with Dust Racing 2D. If not, see <http://www.gnu.org/licenses/>.
 
 #include "ai.hpp"
 #include "car.hpp"
