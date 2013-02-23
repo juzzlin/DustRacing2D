eece967c54180c15e2c54137893ddb131c329a2c
Jussi Lind <jussi.lind@iki.fi>
2013-02-23 12:34:37 +0200

    1.0.2

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,2 +1,2 @@
-git archive --format=tar --prefix=dustrac-1.0.1/ HEAD | gzip > dustrac-1.0.1.tar.gz
-git archive --format=zip --prefix=dustrac-1.0.1/ HEAD > dustrac-1.0.1.zip
+git archive --format=tar --prefix=dustrac-1.0.2/ HEAD | gzip > dustrac-1.0.2.tar.gz
+git archive --format=zip --prefix=dustrac-1.0.2/ HEAD > dustrac-1.0.2.zip
