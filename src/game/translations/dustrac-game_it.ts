6da697a41679c694c0fa2d821801d6e22a5fd8d1
Jussi Lind <jussi.lind@iki.fi>
2013-11-12 21:41:01 +0200

    Add Italian translations.

---
+++ src/game/translations/dustrac-game_it.ts
@@ -0,0 +1,485 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="../mainmenu.cpp" line="36"/>
+        <source>Play</source>
+        <translation>Gioca</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="40"/>
+        <source>Help</source>
+        <translation>Aiuto</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="44"/>
+        <source>Credits</source>
+        <translation>Riconoscimenti</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="48"/>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <location filename="../mainmenu.cpp" line="57"/>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="48"/>
+        <source>At least OpenGL ES 2.0 is required!</source>
+        <translation>È richiesta almeno OpenGL ES 2.0!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="49"/>
+        <location filename="../main.cpp" line="56"/>
+        <location filename="../main.cpp" line="63"/>
+        <source>Cannot start Dust Racing 2D</source>
+        <translation>Impossibile avviare Dust Racing 2D</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="55"/>
+        <source>At least OpenGL 3.0 is required!</source>
+        <translation>È richiesta almeno OpenGL 3.0!</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="62"/>
+        <source>At least OpenGL 2.1 is required!</source>
+        <translation>È richiesta almeno OpenGL 2.1!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="253"/>
+        <source>The Time Trial has ended!</source>
+        <translation>La prova cronometrata è terminata!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="257"/>
+        <source>The winner has finished!</source>
+        <translation>Il vincitore ha finito!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="378"/>
+        <source>New lap record!</source>
+        <translation>Nuovo record sul giro!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="394"/>
+        <source>A new best pos!</source>
+        <translation>Nuova migliore pos!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="404"/>
+        <source>A new track unlocked!</source>
+        <translation>Nuovo tracciato sbloccato!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="408"/>
+        <source>Better luck next time..</source>
+        <translation>La prossima volta sarai piu' fortunato..</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="450"/>
+        <source>You must stay on track!</source>
+        <translation>Devi rimanere sul tracciato!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="454"/>
+        <source>Watch your tires!</source>
+        <translation>Controlla le gomme!</translation>
+    </message>
+    <message>
+        <location filename="../startlights.cpp" line="138"/>
+        <source>GO!!!</source>
+        <translation>VIA!!!</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="40"/>
+        <source>---</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="41"/>
+        <source>1st</source>
+        <translation>1.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="42"/>
+        <source>2nd</source>
+        <translation>2.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="43"/>
+        <source>3rd</source>
+        <translation>3.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="44"/>
+        <source>4th</source>
+        <translation>4.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="45"/>
+        <source>5th</source>
+        <translation>5.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="46"/>
+        <source>6th</source>
+        <translation>6.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="47"/>
+        <source>7th</source>
+        <translation>7.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="48"/>
+        <source>8th</source>
+        <translation>8.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="49"/>
+        <source>9th</source>
+        <translation>9.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="50"/>
+        <source>10th</source>
+        <translation>10.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="51"/>
+        <source>11th</source>
+        <translation>11.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="52"/>
+        <source>12th</source>
+        <translation>12.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="104"/>
+        <source> LAP:</source>
+        <translation> GIRO:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="120"/>
+        <source> POS:</source>
+        <translation> POS:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="127"/>
+        <source>LAP</source>
+        <translation>GIRO</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="127"/>
+        <source>LAPS</source>
+        <translation>GIRI</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="166"/>
+        <source> KM/H</source>
+        <translation> KM/H</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="226"/>
+        <source>L:</source>
+        <translation>G:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="246"/>
+        <source>R:</source>
+        <translation>R:</translation>
+    </message>
+    <message>
+        <location filename="../help.cpp" line="30"/>
+        <source>GAME GOAL
+
+You are racing against eleven
+computer players.
+
+Your best position will be
+the next start position.
+
+Finish in TOP-6 to unlock
+a new race track!
+
+CONTROLS FOR PLAYER 1
+
+Turn left  : Left
+Turn right : Right
+Accelerate : Up / RIGHT SHIFT
+Brake      : Down / RIGHT CTRL
+
+CONTROLS FOR PLAYER 2
+
+Turn left  : A
+Turn right : D
+Accelerate : W / LEFT SHIFT
+Brake      : S / LEFT CTRL
+
+Quit       : ESC/Q
+Pause      : P
+
+http://dustrac.sourceforge.net</source>
+        <translation>OBIETTIVO DEL GIOCO
+
+Corri contro undici giocatori
+controllati dal computer.
+
+La tua migliore posizione
+sarà quella della partenza
+successiva.
+
+Arriva tra i primi 6 per
+sbloccare un nuovo tracciato!
+
+CONTROLLI GIOCATORE 1
+Gira a sinistra  : Sinistra
+Gira a destra    : Destra
+Accelera   : Su / MAIUSC DESTRO
+Frena      : Giù / CTRL DESTRO
+
+CONTROLLI GIOCATORE 2
+Gira a sinistra  : A
+Gira a destra    : D
+Accelera   : W / MAIUSC SINISTRO
+Frena      : S / CTRL SINISTRO
+
+Esci       : ESC/Q
+Pause      : P
+
+http://dustrac.sourceforge.net</translation>
+    </message>
+    <message>
+        <location filename="../credits.cpp" line="30"/>
+        <source>PROGRAMMING BY
+
+* Jussi Lind
+
+GRAPHICS BY
+
+* Jussi Lind
+* Ville Makiranta
+* FindIcons.com
+
+RACE TRACKS BY
+
+* Jussi Lind
+
+FONT
+
+* Ubuntu Monospace
+
+PATCHES
+
+* Markus Rinne
+
+SPECIAL THANKS
+
+* Tommi Martela</source>
+        <translation>PROGRAMMATO DA
+
+* Jussi Lind
+
+GRAFICA
+
+* Jussi Lind
+* Ville Makiranta
+* FindIcons.com
+
+TRACCIATI
+
+* Jussi Lind
+
+FONT
+
+* Ubuntu Monospace
+
+PATCH
+
+* Markus Rinne
+
+RINGRAZIAMENTI SPECIALI
+
+* Tommi Martela</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="60"/>
+        <source>Reset best positions?</source>
+        <translation>Azzerare migliori posizioni?</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="72"/>
+        <source>Reset record times?</source>
+        <translation>Azzerare tempi record?</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="84"/>
+        <source>Reset unlocked tracks?</source>
+        <translation>Azzerare tracciati sbloccati?</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="158"/>
+        <source>Reset record times</source>
+        <translation>Azzera tempi record</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="163"/>
+        <source>Reset best positions</source>
+        <translation>Azzerare migliori posizioni</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="168"/>
+        <source>Reset unlocked tracks</source>
+        <translation>Azzerare tracciati sbloccati</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="173"/>
+        <source>Full screen resolution &gt;</source>
+        <translation>Risoluzione a schermo intero &gt;</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="177"/>
+        <source>Windowed resolution &gt;</source>
+        <translation>Risoluzione in finestra &gt;</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="181"/>
+        <source>Split type &gt;</source>
+        <translation>Divisione schermo&gt;</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="185"/>
+        <source>Game mode &gt;</source>
+        <translation>Modalità di gioco &gt; </translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="189"/>
+        <source>Lap Count &gt;</source>
+        <translation>Numero di giri &gt;</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="193"/>
+        <source>FPS &gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="197"/>
+        <source>Key configuration &gt;</source>
+        <translation>Configurazione tasti &gt;</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="221"/>
+        <source>Two player race</source>
+        <translation>Gareggia con due giocatori</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="231"/>
+        <source>One player race</source>
+        <translation>Gareggia con un giocatore</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="241"/>
+        <source>Time Trial</source>
+        <translation>Prova cronometrata</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="251"/>
+        <source>Duel</source>
+        <translation>Duello</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="275"/>
+        <source>Vertical</source>
+        <translation>Verticale</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="285"/>
+        <source>Horizontal</source>
+        <translation>Orizzontale</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="307"/>
+        <source>30 fps</source>
+        <translation>30 fps</translation>
+    </message>
+    <message>
+        <location filename="../settingsmenu.cpp" line="318"/>
+        <source>60 fps</source>
+        <translation>60 fps</translation>
+    </message>
+    <message>
+        <location filename="../trackselectionmenu.cpp" line="226"/>
+        <source>  Laps: </source>
+        <translation>  Giri: </translation>
+    </message>
+    <message>
+        <location filename="../trackselectionmenu.cpp" line="232"/>
+        <source>Length: </source>
+        <translation>Lunghezza: </translation>
+    </message>
+    <message>
+        <location filename="../trackselectionmenu.cpp" line="234"/>
+        <source> m</source>
+        <translation> m</translation>
+    </message>
+    <message>
+        <location filename="../trackselectionmenu.cpp" line="241"/>
+        <source>Record: </source>
+        <translation>Record: </translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="52"/>
+        <source>Press a key..</source>
+        <translation>Premi un tasto..</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="93"/>
+        <source>Player One Accelerate</source>
+        <translation>Il giocatore 1 accelera</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="103"/>
+        <source>Player One Brake</source>
+        <translation>Il giocatore 1 frena</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="113"/>
+        <source>Player One Turn Left</source>
+        <translation>Il giocatore 1 gira a sinistra</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="123"/>
+        <source>Player One Turn Right</source>
+        <translation>Il giocatore 1 gira a destra</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="146"/>
+        <source>Player Two Accelerate</source>
+        <translation>Il giocatore 2 accelera</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="156"/>
+        <source>Player Two Brake</source>
+        <translation>Il giocatore 2 frena</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="166"/>
+        <source>Player Two Turn Left</source>
+        <translation>Il giocatore 2 gira a sinistra</translation>
+    </message>
+    <message>
+        <location filename="../keyconfigmenu.cpp" line="176"/>
+        <source>Player Two Turn Right</source>
+        <translation>Il giocatore 2 gira a destra</translation>
+    </message>
+</context>
+</TS>
