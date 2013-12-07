b1dc512e78eec7a3058c5409c7f3ed6348d5e6c7
Jussi Lind <jussi.lind@iki.fi>
2013-12-08 00:06:10 +0200

    Add install target for fonts.

--- InstallLinux.cmake
+++ InstallLinux.cmake
@@ -64,6 +64,7 @@ function(setup_install_targets BIN_PATH DATA_PATH DOC_PATH)
     install(DIRECTORY data/levels DESTINATION ${DATA_PATH} FILES_MATCHING PATTERN "*.trk")
     install(DIRECTORY data/models DESTINATION ${DATA_PATH} FILES_MATCHING PATTERN "*.obj")
     install(DIRECTORY data/sounds DESTINATION ${DATA_PATH} FILES_MATCHING PATTERN "*.ogg")
+    install(DIRECTORY data/fonts DESTINATION ${DATA_PATH} FILES_MATCHING PATTERN "*.ttf")
     install(DIRECTORY data/translations DESTINATION ${DATA_PATH} FILES_MATCHING PATTERN "*.qm")
 
     if(ReleaseBuild)
