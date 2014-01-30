211701c96b156459a98957b327dc2b7c363add79
Jussi Lind <jussi.lind@iki.fi>
2014-01-30 19:12:47 +0200

    WWW: Fix image URLs.

--- web/app.js
+++ web/app.js
@@ -1,6 +1,6 @@
 var ImageCtrl = function ($scope) {
-    $scope.image1 = "screenshots/1.6.3/1.jpg";
-    $scope.image2 = "screenshots/1.6.3/2.jpg";
-    $scope.image3 = "screenshots/1.6.3/3.jpg";
+    $scope.image1 = 'screenshots/1.6.3/1.jpg';
+    $scope.image2 = 'screenshots/1.6.3/2.jpg';
+    $scope.image3 = 'screenshots/1.6.3/3.jpg';
 }
 
