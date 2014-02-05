f0f02b6807fa1107b220a0a6da55ea6c59b39691
Jussi Lind <jussi.lind@iki.fi>
2014-02-06 00:15:26 +0200

    Increase the initial slide friction to 1.0

--- src/game/car.hpp
+++ src/game/car.hpp
@@ -49,7 +49,7 @@ public:
         , rollingFrictionOnTrack(0.1)
         , rollingFrictionOffTrack(0.75)
         , rotationFriction(1.0)
-        , slideFriction(0.90)
+        , slideFriction(1.0)
         , turningImpulse(0.28)
         , power(5000.0)
         , mass(1000.0)
