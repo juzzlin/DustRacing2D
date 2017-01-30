964ec34a3aaef46a6570cb0257bda3e9cb8b0254
Jussi Lind <jussi.lind@iki.fi>
2017-01-31 01:47:41 +0200

    Use Gulp to bundle scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
 const SASS_INCLUDE_PATHS = ['./node_modules/foundation-sites/scss/', './node_modules/motion-ui/']
 
@@ -8,3 +9,23 @@ gulp.task('sass', function() {
         .pipe(sass({includePaths: SASS_INCLUDE_PATHS}).on('error', sass.logError))
         .pipe(gulp.dest('dist/css'));
 });
+
+const SCRIPTS = [
+    "node_modules/jquery/dist/jquery.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.core.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.util.timerAndImageLoader.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.util.touch.min.js",
+    "node_modules/foundation-sites/dist/js/plugins/foundation.orbit.min.js",
+    "node_modules/motion-ui/dist/motion-ui.min.js"];
+
+gulp.task('scripts', function() {
+    return gulp.src(SCRIPTS)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+// Default task
+gulp.task('default', ['sass', 'scripts']);
