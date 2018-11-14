ea676c76ffac31bdfeb94bdc87fbc5ec876c5db2
Jussi Lind <jussi.lind@iki.fi>
2018-11-15 00:54:17 +0200

    Use new-style signals in rotatedialog.cpp

--- src/editor/rotatedialog.cpp
+++ src/editor/rotatedialog.cpp
@@ -42,8 +42,8 @@ RotateDialog::RotateDialog(QWidget * parent)
     m_layout->addWidget(m_okButton,     3, 0);
     m_layout->addWidget(m_cancelButton, 3, 1);
 
-    connect(m_okButton, SIGNAL(clicked()), this, SLOT(accept()));
-    connect(m_cancelButton, SIGNAL(clicked()), this, SLOT(reject()));
+    connect(m_okButton, &QPushButton::clicked, this, &RotateDialog::accept);
+    connect(m_cancelButton, &QPushButton::clicked, this, &RotateDialog::reject);
 }
 
 int RotateDialog::angle() const
