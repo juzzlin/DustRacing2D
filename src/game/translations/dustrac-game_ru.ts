42bef06fcd4461973a485ec794820ccf484075e6
a.puskin <andreipuskin@gmail.com>
2019-09-20 12:56:22 +0300

    Added translation files for the Russian language

---
+++ src/game/translations/dustrac-game_ru.ts
@@ -0,0 +1,448 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Начать</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <source>Credits</source>
+        <translation>Разработчики</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source> m</source>
+        <translation> m</translation>
+    </message>
+    <message>
+        <source>L:</source>
+        <translation>Время круга:</translation>
+    </message>
+    <message>
+        <source>R:</source>
+        <translation>Время заезда:</translation>
+    </message>
+    <message>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <source>On</source>
+        <translation>Вкл</translation>
+    </message>
+    <message>
+        <source>---</source>
+        <translation>---</translation>
+    </message>
+    <message>
+        <source>1st</source>
+        <translation>1й</translation>
+    </message>
+    <message>
+        <source>2nd</source>
+        <translation>2й</translation>
+    </message>
+    <message>
+        <source>3rd</source>
+        <translation>3й</translation>
+    </message>
+    <message>
+        <source>4th</source>
+        <translation>4й</translation>
+    </message>
+    <message>
+        <source>5th</source>
+        <translation>5й</translation>
+    </message>
+    <message>
+        <source>6th</source>
+        <translation>6й</translation>
+    </message>
+    <message>
+        <source>7th</source>
+        <translation>7й</translation>
+    </message>
+    <message>
+        <source>8th</source>
+        <translation>8й</translation>
+    </message>
+    <message>
+        <source>9th</source>
+        <translation>9й</translation>
+    </message>
+    <message>
+        <source>LAP</source>
+        <translation>Круг</translation>
+    </message>
+    <message>
+        <source>Off</source>
+        <translation>Выкл</translation>
+    </message>
+    <message>
+        <source>10th</source>
+        <translation>10й</translation>
+    </message>
+    <message>
+        <source>11th</source>
+        <translation>11й</translation>
+    </message>
+    <message>
+        <source>12th</source>
+        <translation>12й</translation>
+    </message>
+    <message>
+        <source>Easy</source>
+        <translation>Легкий</translation>
+    </message>
+    <message>
+        <source>Duel</source>
+        <translation>Дуэль</translation>
+    </message>
+    <message>
+        <source>LAP:</source>
+        <translation>Круг: </translation>
+    </message>
+    <message>
+        <source>LAPS</source>
+        <translation>Круги</translation>
+    </message>
+    <message>
+        <source>TOT:</source>
+        <translation>Общее.: </translation>
+    </message>
+    <message>
+        <source>RACE TRACKS BY</source>
+        <translation>Трассы</translation>
+    </message>
+    <message>
+        <source> KM/H</source>
+        <translation> км/ч</translation>
+    </message>
+    <message>
+        <source> LAP:</source>
+        <translation> Круг: </translation>
+    </message>
+    <message>
+        <source> POS:</source>
+        <translation> Позиция: </translation>
+    </message>
+    <message>
+        <source>GFX &gt;</source>
+        <translation>Настройки графики &gt;</translation>
+    </message>
+    <message>
+        <source>GO!!!</source>
+        <translation>Вперед!</translation>
+    </message>
+    <message>
+        <source>Hard</source>
+        <translation>Тяжёлый</translation>
+    </message>
+    <message>
+        <source>Horizontal</source>
+        <translation>По горизонтали</translation>
+    </message>
+    <message>
+        <source>SPECIAL THANKS</source>
+        <translation>БЛАГОДАРНОСТИ</translation>
+    </message>
+    <message>
+        <source>GAME GOAL
+
+You are racing against eleven
+computer players.
+
+Your best position will be
+the next start position.
+
+Finish in TOP-6 to unlock
+a new race track!
+
+CONTROLS FOR PLAYER 1
+
+Turn left  : Left
+Turn right : Right
+Accelerate : Up / RIGHT SHIFT
+Brake      : Down / RIGHT CTRL
+
+CONTROLS FOR PLAYER 2
+
+Turn left  : A
+Turn right : D
+Accelerate : W / LEFT SHIFT
+Brake      : S / LEFT CTRL
+
+Quit       : ESC/Q
+Pause      : P</source>
+        <translation>ПРАВИЛА ИГРЫ
+
+Вы участвуете в гонке против
+двенадцати компьютерных игроков.
+
+Ваша лучшая позиция в текущей
+гонке становится начальной
+позицией в следующей.
+
+Следующая трасса разблокируется
+при поподании в топ 6 игроков
+для текущей трассы.
+
+УПРАВЛЕНИЕ ИГРОКА №1
+
+Влево      : Стрелка влево
+Вправо     : Стрелка вправо
+Газ        : Стрелка вверх / правый SHIFT
+Тормоз     : Стрелка вниз / правый CTRL
+
+УПРАВЛЕНИЕ ИГРОКА №2
+
+Влево      : A
+Вправо     : D
+Газ        : W / левый SHIFT
+Тормоз     : S / левый CTRL
+
+Выход      : ESC/Q
+Пауза      : P</translation>
+    </message>
+    <message>
+        <source>Reset unlocked tracks?</source>
+        <translation>Сбросить разблокированные трассы?</translation>
+    </message>
+    <message>
+        <source>New lap record!</source>
+        <translation>Новый рекорд круга!</translation>
+    </message>
+    <message>
+        <source>Player One Turn Left</source>
+        <translation>Игрок 1: Влево</translation>
+    </message>
+    <message>
+        <source>Restart to change the resolution.</source>
+        <translation>Перезапустить, для смены разрешения экрана.</translation>
+    </message>
+    <message>
+        <source>A new track unlocked!</source>
+        <translation>Новая трасса разблокирована!</translation>
+    </message>
+    <message>
+        <source>Split type &gt;</source>
+        <translation>Разделять экран для двух игроков &gt;</translation>
+    </message>
+    <message>
+        <source>Player Two Turn Left</source>
+        <translation>Игрок 2: Влево</translation>
+    </message>
+    <message>
+        <source>The winner has finished!</source>
+        <translation>Победитель на финишной прямой!</translation>
+    </message>
+    <message>
+        <source>Reset best positions?</source>
+        <translation>Сбросить лучшие результаты?</translation>
+    </message>
+    <message>
+        <source>Race Record: </source>
+        <translation>Рекорд трассы: </translation>
+    </message>
+    <message>
+        <source>Reset record times</source>
+        <translation>Сбросить рекорды на время</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <source>Press a key..</source>
+        <translation>Нажмите любую клавишу..</translation>
+    </message>
+    <message>
+        <source>Medium</source>
+        <translation>Средний</translation>
+    </message>
+    <message>
+        <source>Full screen resolution &gt;</source>
+        <translation>Полный экран &gt;</translation>
+    </message>
+    <message>
+        <source>Pit stop!</source>
+        <translation>Пит стоп!</translation>
+    </message>
+    <message>
+        <source>Player Two Turn Right</source>
+        <translation>Игрок 2: Вправо</translation>
+    </message>
+    <message>
+        <source>Sounds &gt;</source>
+        <translation>Звук &gt;</translation>
+    </message>
+    <message>
+        <source>Better luck next time..</source>
+        <translation>Повезет в следующий раз..</translation>
+    </message>
+    <message>
+        <source>PROGRAMMING BY</source>
+        <translation>ПРОГРАММИРОВАНИЕ</translation>
+    </message>
+    <message>
+        <source>       Laps: </source>
+        <translation>       Круги: </translation>
+    </message>
+    <message>
+        <source>Time Trial</source>
+        <translation>Пробный заезд</translation>
+    </message>
+    <message>
+        <source>Game mode &gt;</source>
+        <translation>Режим игры &gt;</translation>
+    </message>
+    <message>
+        <source>Reset record times?</source>
+        <translation>Сбросить рекорды на время?</translation>
+    </message>
+    <message>
+        <source>Player One Brake</source>
+        <translation>Игрок 1: Тормоз</translation>
+    </message>
+    <message>
+        <source>Player One Accelerate</source>
+        <translation>Игрок 1: Газ</translation>
+    </message>
+    <message>
+        <source>Reset &gt;</source>
+        <translation>Сброс &gt;</translation>
+    </message>
+    <message>
+        <source>The Time Trial has ended!</source>
+        <translation>Пробный заезд закончился!</translation>
+    </message>
+    <message>
+        <source>You must stay on track!</source>
+        <translation>Вернитесь на трассу!</translation>
+    </message>
+    <message>
+        <source>CONTRIBUTORS</source>
+        <translation>УЧАСТНИКИ</translation>
+    </message>
+    <message>
+        <source>Reset best positions</source>
+        <translation>Сбросить лучшие рекорды</translation>
+    </message>
+    <message>
+        <source>Lap Count &gt;</source>
+        <translation>Количество кругов &gt;</translation>
+    </message>
+    <message>
+        <source>     Length: </source>
+        <translation>     Длина: </translation>
+    </message>
+    <message>
+        <source>Restart to change VSync setting.</source>
+        <translation>Перезапустить для изменения настроек вертикальной синхронизации.</translation>
+    </message>
+    <message>
+        <source>Reset unlocked tracks</source>
+        <translation>Сбросить разблокированные трассы</translation>
+    </message>
+    <message>
+        <source>One player race</source>
+        <translation>Заезд для одного игрока</translation>
+    </message>
+    <message>
+        <source>Controls &gt;</source>
+        <translation>Управление &gt;</translation>
+    </message>
+    <message>
+        <source>VSync &gt;</source>
+        <translation>Вертикальная синхронизация &gt;</translation>
+    </message>
+    <message>
+        <source>GRAPHICS BY</source>
+        <translation>ГРАФИЧЕСКОЕ ОФОРМЛЕНИЕ</translation>
+    </message>
+    <message>
+        <source>Vertical</source>
+        <translation>По вертикали</translation>
+    </message>
+    <message>
+        <source>Player One Turn Right</source>
+        <translation>Игрок 1: Вправо</translation>
+    </message>
+    <message>
+        <source>Two player race</source>
+        <translation>Заезд для двух игроков</translation>
+    </message>
+    <message>
+        <source>Watch your tires!</source>
+        <translation>Смотри куда едешь!</translation>
+    </message>
+    <message>
+        <source>Difficulty &gt;</source>
+        <translation>Сложность &gt;</translation>
+    </message>
+    <message>
+        <source>Player Two Brake</source>
+        <translation>Игрок 2: Тормоз</translation>
+    </message>
+    <message>
+        <source>A new best pos!</source>
+        <translation>Новое лучшее место!</translation>
+    </message>
+    <message>
+        <source>Player Two Accelerate</source>
+        <translation>Игрок 2: Газ</translation>
+    </message>
+    <message>
+        <source> Lap Record: </source>
+        <translation> Рекорд круга: </translation>
+    </message>
+    <message>
+        <source>New race record!</source>
+        <translation>Новый рекорд гонки!</translation>
+    </message>
+    <message>
+        <source>Windowed resolution &gt;</source>
+        <translation>Разрешение окна &gt;</translation>
+    </message>
+    <message>
+        <source>PROGRAMMING</source>
+        <translation>ПРОГРАММИРОВАНИЕ</translation>
+    </message>
+    <message>
+        <source>GRAPHICS</source>
+        <translation>Графика</translation>
+    </message>
+    <message>
+        <source>LEVEL DESIGN</source>
+        <translation>ДИЗАЙН УРОВНЕЙ</translation>
+    </message>
+    <message>
+        <source>TRANSLATIONS</source>
+        <translation>ПЕРЕВОДЧИКИ</translation>
+    </message>
+    <message>
+        <source>PATCHES</source>
+        <translation>ДОПОЛНЕНИЯ</translation>
+    </message>
+    <message>
+        <source>Choose lap count</source>
+        <translation>Выберете количество кругов</translation>
+    </message>
+</context>
+</TS>
