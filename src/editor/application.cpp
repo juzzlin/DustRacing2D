903a775cfd7088be600061cff4ce422d24fd9d8f
Jussi Lind <jussi.lind@iki.fi>
2019-09-20 18:20:07 +0300

    Add ru to editor's usage

--- src/editor/application.cpp
+++ src/editor/application.cpp
@@ -31,7 +31,7 @@ static void printHelp()
     std::cout << "Usage: dustrac-editor [options] [trackFile]" << std::endl << std::endl;
     std::cout << "Options:" << std::endl;
     std::cout << "--help        Show this help." << std::endl;
-    std::cout << "--lang [lang] Force language: fi, fr, it, cs." << std::endl;
+    std::cout << "--lang [lang] Force language: fi, fr, it, cs, ru." << std::endl;
     std::cout << std::endl;
 }
 
