292a441ab7566b97ae504434169a72dab87b1094
Jussi Lind <jussi.lind@iki.fi>
2019-09-20 19:17:57 +0300

    Fix language in editor/translations/dustrac-editor_ru.ts

--- src/editor/translations/dustrac-editor_ru.ts
+++ src/editor/translations/dustrac-editor_ru.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="de_DE" sourcelanguage="en">
+<TS version="2.1" language="ru_RU" sourcelanguage="en">
 <context>
     <name>AboutDlg</name>
     <message>
