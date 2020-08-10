1c103c48762f4c741118fb36e0d40b67339774ff
Heimen Stoffels <vistausss@outlook.com>
2020-08-10 18:21:11 +0200

    Added Dutch translation

---
+++ src/editor/translations/dustrac-editor_nl.ts
@@ -0,0 +1,500 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>AboutDlg</name>
+    <message>
+        <location filename="../aboutdlg.cpp" line="28"/>
+        <source>About Dust Racing</source>
+        <translation>Over Dust Racing</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="65"/>
+        <source>Randomly rotate objects</source>
+        <translation>Objecten willekeurig draaien</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="93"/>
+        <source>Choose &apos;File -&gt; New&apos; or &apos;File -&gt; Open&apos; to start.</source>
+        <translation>Klik op &apos;Bestand --&gt; Nieuw&apos; of &apos;Bestand --&gt; Openen&apos; om aan de slag te gaan.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="99"/>
+        <source>Loading &apos;%1&apos;..</source>
+        <translation>Bezig met laden van &apos;%1&apos;...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <source>Save your work to correct location.</source>
+        <translation>Sla je werk op op de juiste locatie.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <location filename="../mainwindow.cpp" line="134"/>
+        <source>To make the game find your circuits, save them to %1%2%3%2</source>
+        <translation>Sla je werk op in %1%2%3%2 zodat het spel de circuits kan laden.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <source>, %4%2</source>
+        <translation>, %4%2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <location filename="../mainwindow.cpp" line="631"/>
+        <source>New file</source>
+        <translation>Nieuw bestand</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Scale:</source>
+        <translation>Schaal:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <source>ERROR!!: Cannot load objects from &apos;</source>
+        <translation>FOUT!! Er kunnen geen objecten worden geladen uit &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <source>&apos;</source>
+        <translation>&apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="291"/>
+        <source>Scale set to %1%</source>
+        <translation>Schaal instellen op %1%</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="310"/>
+        <source>&amp;File</source>
+        <translation>&amp;Bestand</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="313"/>
+        <source>&amp;New...</source>
+        <translation>&amp;Nieuw...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="319"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Openen...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <source>&amp;Save</source>
+        <translation>Op&amp;slaan</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="332"/>
+        <source>&amp;Save as...</source>
+        <translation>Op&amp;slaan als...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="339"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Afsluiten</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="345"/>
+        <source>&amp;Edit</source>
+        <translation>B&amp;ewerken</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="348"/>
+        <source>Undo</source>
+        <translation>Ongedaan maken</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="362"/>
+        <source>Redo</source>
+        <translation>Opnieuw uitvoeren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="376"/>
+        <source>&amp;Set properties..</source>
+        <translation>Kenmerken in&amp;stellen...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="382"/>
+        <source>&amp;Route</source>
+        <translation>&amp;Route</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="385"/>
+        <source>Clear &amp;route</source>
+        <translation>&amp;Route wissen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="391"/>
+        <source>&amp;Set route..</source>
+        <translation>Route in&amp;stellen...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="397"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Hulp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="400"/>
+        <source>&amp;About</source>
+        <translation>&amp;Over</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="405"/>
+        <source>About &amp;Qt</source>
+        <translation>Over &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="413"/>
+        <source>Select</source>
+        <translation>Selecteren</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="419"/>
+        <source>Erase object</source>
+        <translation>Object wissen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="424"/>
+        <source>Clear</source>
+        <translation>Wissen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="498"/>
+        <source>Open a track</source>
+        <translation>Circuit openen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="498"/>
+        <location filename="../mainwindow.cpp" line="678"/>
+        <source>Track Files (*.trk)</source>
+        <translation>Circuitbestanden (*.trk)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="512"/>
+        <source>About Qt</source>
+        <translation>Over Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="526"/>
+        <source>ERROR!!: %1 doesn&apos;t exist.</source>
+        <translation>FOUT!!: %1 bestaat niet.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="536"/>
+        <source>Track &apos;%1&apos; opened.</source>
+        <translation>Circuit &apos;%1&apos; geopend.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="560"/>
+        <source>Failed to open track &apos;</source>
+        <translation>Het volgende circuit kan niet worden geopend: &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="568"/>
+        <source>Set route: begin.</source>
+        <translation>Aan de slag.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <source>Set route, checkpoints and driving lines.</source>
+        <translation>Route, controlepunten en computertrajecten instellen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="572"/>
+        <source>Setting the route defines checkpoints for the cars so
+that no shortcuts can be taken. It also defines
+driving lines for the computer players.
+
+Click on the tiles one by one and make a closed loop
+with the target nodes. You can adjust the nodes afterwads.
+Start from the first tile after the finish line tile
+to make the lap detection and timing work correctly.
+Click on the first node again to finish.</source>
+        <translation>De route bepaalt de controlepunten van de auto&apos;s zodat
+er niet kan worden afgesneden. Het bepaalt tevens het
+traject dat de computerspelers afleggen.
+
+Klik één voor één op de tegels en maak middels de knopen
+een gesloten lus. Je kunt de knopen achteraf verschuiven.
+Begin op de eerste tegel na de finish om het rondedetectie- en
+tijdsysteem goed te laten werken. Klik wederom op de eerste
+knoop om af te ronden.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="583"/>
+        <source>Set route: not a valid track.</source>
+        <translation>Geen geldig circuit.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="584"/>
+        <source>Set route</source>
+        <translation>Route instellen</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="584"/>
+        <source>Invalid track. Route cannot be set.</source>
+        <translation>Ongeldig circuit. De route kan niet worden ingesteld.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="610"/>
+        <source>Set route: route finished.</source>
+        <translation>Route afgerond.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="637"/>
+        <source>A new track &apos;%1&apos; created. Columns: %2, Rows: %3.</source>
+        <translation>Je hebt een nieuw circuit, &apos;%1&apos;, gemaakt met %2 kolommen en %3 rijen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="658"/>
+        <location filename="../mainwindow.cpp" line="688"/>
+        <source>Track &apos;</source>
+        <translation>Circuit &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="658"/>
+        <location filename="../mainwindow.cpp" line="688"/>
+        <source>&apos; saved.</source>
+        <translation>&apos; is opgeslagen.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="664"/>
+        <source>Failed to save track &apos;</source>
+        <translation>Het volgende circuit kan niet worden opgeslagen: &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="676"/>
+        <source>Save a track</source>
+        <translation>Circuit opslaan</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="694"/>
+        <source>Failed to save track as &apos;</source>
+        <translation>Het circuit kan niet worden opgeslagen als: &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="724"/>
+        <source>Track properties updated.</source>
+        <translation>De circuitkenmerken zijn bijgewerkt.</translation>
+    </message>
+</context>
+<context>
+    <name>NewTrackDialog</name>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="29"/>
+        <source>Ok</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="30"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="34"/>
+        <source>Number of tile columns:</source>
+        <translation>Aantal tegelkolommen:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="35"/>
+        <source>Number of tile rows:</source>
+        <translation>Aantal tegelrijen:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="36"/>
+        <source>Track name:</source>
+        <translation>Circuitnaam:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="37"/>
+        <source>User track:</source>
+        <translation>Gebruikerscircuit:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="40"/>
+        <source>Create a new track</source>
+        <translation>Nieuw circuit maken</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../editordata.cpp" line="377"/>
+        <source>Route cleared.</source>
+        <translation>De route is gewist.</translation>
+    </message>
+</context>
+<context>
+    <name>QWidget</name>
+    <message>
+        <location filename="../editorview.cpp" line="95"/>
+        <location filename="../editorview.cpp" line="107"/>
+        <source>Rotate 90</source>
+        <translation>90 graden draaien</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="95"/>
+        <source> CW..</source>
+        <translation> Met de klok mee...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="107"/>
+        <source> CCW..</source>
+        <translation> Tegen de klok in...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="119"/>
+        <source>Clear computer hint</source>
+        <translation>Computeropdracht wissen</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="126"/>
+        <source>Set computer hint &apos;brake hard&apos;..</source>
+        <translation>Computeropdracht &apos;hard remmen&apos; instellen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="133"/>
+        <source>Set computer hint &apos;brake&apos;..</source>
+        <translation>Computeropdracht &apos;remmen&apos; instellen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="140"/>
+        <source>Exclude from minimap</source>
+        <translation>Niet tonen op minikaart</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="151"/>
+        <source>Insert row before..</source>
+        <translation>Rij hiervoor invoegen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="160"/>
+        <source>Insert row after..</source>
+        <translation>Rij hierna invoegen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="169"/>
+        <source>Delete row..</source>
+        <translation>Rij verwijderen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="178"/>
+        <source>Insert column before..</source>
+        <translation>Kolom hiervoor invoegen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="187"/>
+        <source>Insert column after..</source>
+        <translation>Kolom hierna invoegen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="196"/>
+        <source>Delete column..</source>
+        <translation>Kolom verwijderen...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="225"/>
+        <source>Rotate..</source>
+        <translation>Draaien...</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="240"/>
+        <source>Force stationary</source>
+        <translation>Stationair afdwingen</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="259"/>
+        <source>Set size..</source>
+        <translation>Grootte instellen...</translation>
+    </message>
+</context>
+<context>
+    <name>RotateDialog</name>
+    <message>
+        <location filename="../rotatedialog.cpp" line="28"/>
+        <source>Ok</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../rotatedialog.cpp" line="29"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../rotatedialog.cpp" line="31"/>
+        <source>Angle in degrees:</source>
+        <translation>Hoek (in graden):</translation>
+    </message>
+    <message>
+        <location filename="../rotatedialog.cpp" line="33"/>
+        <source>Rotate Object</source>
+        <translation>Object draaien</translation>
+    </message>
+</context>
+<context>
+    <name>TargetNodeSizeDlg</name>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="29"/>
+        <source>Ok</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="30"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="32"/>
+        <source>Width:</source>
+        <translation>Breedte:</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="34"/>
+        <source>Height:</source>
+        <translation>Hoogte:</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="36"/>
+        <source>Set target node size</source>
+        <translation>Knoopgrootte instellen</translation>
+    </message>
+</context>
+<context>
+    <name>TrackPropertiesDialog</name>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="30"/>
+        <source>Ok</source>
+        <translation>Oké</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="31"/>
+        <source>Cancel</source>
+        <translation>Annuleren</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="33"/>
+        <source>Name:</source>
+        <translation>Naam:</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="35"/>
+        <source>Index:</source>
+        <translation>Index:</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="37"/>
+        <source>User track:</source>
+        <translation>Gebruikerscircuit:</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="39"/>
+        <source>Set track properties</source>
+        <translation>Circuitkenmerken instellen</translation>
+    </message>
+</context>
+</TS>
