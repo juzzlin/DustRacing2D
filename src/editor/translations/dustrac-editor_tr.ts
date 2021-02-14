530e5c76f7877051b73dfc67991457a55e6635fe
Samet 732 <samethub732@gmail.com>
2021-02-15 02:55:53 +0300

    added Turkish editor translations

---
+++ src/editor/translations/dustrac-editor_tr.ts
@@ -0,0 +1,500 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>AboutDlg</name>
+    <message>
+        <location filename="../aboutdlg.cpp" line="28"/>
+        <source>About Dust Racing</source>
+        <translation>Dust Racing Hakkinda</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="65"/>
+        <source>Randomly rotate objects</source>
+        <translation>Nesneleri rastgele döndür</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="93"/>
+        <source>Choose &apos;File -&gt; New&apos; or &apos;File -&gt; Open&apos; to start.</source>
+        <translation>Baslamak için &apos;Dosya -&gt; Yeni&apos; veya &apos;Dosya -&gt; Aç&apos;i seçin.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="99"/>
+        <source>Loading &apos;%1&apos;..</source>
+        <translation>Yükleniyor &apos;%1&apos;..</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <source>Save your work to correct location.</source>
+        <translation>Çalismanizi dogru konuma kaydedin.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <location filename="../mainwindow.cpp" line="134"/>
+        <source>To make the game find your circuits, save them to %1%2%3%2</source>
+        <translation>Oyunun çalismalarinizi bulmasini saglamak için bunlari %1%2%3%2&apos;ye kaydedin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="125"/>
+        <source>, %4%2</source>
+        <translation>, %4%2</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="143"/>
+        <location filename="../mainwindow.cpp" line="631"/>
+        <source>New file</source>
+        <translation>Yeni dosya</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="189"/>
+        <source>Scale:</source>
+        <translation>Ölçek:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <source>ERROR!!: Cannot load objects from &apos;</source>
+        <translation>HATA !!: Suradan nesneler yüklenemiyor &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <source>&apos;</source>
+        <translation>&apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="291"/>
+        <source>Scale set to %1%</source>
+        <translation>Ölçek su olarak ayarlandi; %1%</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="310"/>
+        <source>&amp;File</source>
+        <translation>&amp;Dosya</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="313"/>
+        <source>&amp;New...</source>
+        <translation>&amp;Yeni...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="319"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Aç...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="325"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Kaydet</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="332"/>
+        <source>&amp;Save as...</source>
+        <translation>&amp;Farkli kaydet...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="339"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Çikis</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="345"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Düzenle</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="348"/>
+        <source>Undo</source>
+        <translation>Geri al</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="362"/>
+        <source>Redo</source>
+        <translation>Ileri al</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="376"/>
+        <source>&amp;Set properties..</source>
+        <translation>&amp;Özellikleri ayarla..</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="382"/>
+        <source>&amp;Route</source>
+        <translation>&amp;Rota</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="385"/>
+        <source>Clear &amp;route</source>
+        <translation>Temizle &amp;rota</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="391"/>
+        <source>&amp;Set route..</source>
+        <translation>&amp;Ayarla rota..</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="397"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Yardim</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="400"/>
+        <source>&amp;About</source>
+        <translation>&amp;Hakkinda</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="405"/>
+        <source>About &amp;Qt</source>
+        <translation>Hakkinda &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="413"/>
+        <source>Select</source>
+        <translation>Seç</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="419"/>
+        <source>Erase object</source>
+        <translation>Nesneyi sil</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="424"/>
+        <source>Clear</source>
+        <translation>Temizle</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="498"/>
+        <source>Open a track</source>
+        <translation>Bir parkur aç</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="498"/>
+        <location filename="../mainwindow.cpp" line="678"/>
+        <source>Track Files (*.trk)</source>
+        <translation>Parkur Dosyalari</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="512"/>
+        <source>About Qt</source>
+        <translation>Qt Hakkinda</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="526"/>
+        <source>ERROR!!: %1 doesn&apos;t exist.</source>
+        <translation>HATA!!: %1 yok.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="536"/>
+        <source>Track &apos;%1&apos; opened.</source>
+        <translation>Parkur &apos;%1&apos; opened.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="560"/>
+        <source>Failed to open track &apos;</source>
+        <translation>Parkur dosyasi acilamadi &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="568"/>
+        <source>Set route: begin.</source>
+        <translation>Rota belirle: Basla.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="571"/>
+        <source>Set route, checkpoints and driving lines.</source>
+        <translation>Rotayi, kontrol noktalarini ve surus hatlarini ayarlayin.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="572"/>
+        <source>Setting the route defines checkpoints for the cars so
+that no shortcuts can be taken. It also defines
+driving lines for the computer players.
+
+Click on the tiles one by one and make a closed loop
+with the target nodes. You can adjust the nodes afterwads.
+Start from the first tile after the finish line tile
+to make the lap detection and timing work correctly.
+Click on the first node again to finish.</source>
+        <translation>Rotayi ayarlamak, arabalar için kontrol noktalarini tanimlar, böylece
+hiçbir kisayol kullanilamaz. Ayrica bilgisayar
+oyunculari için surus hatlari tanimlar.
+
+Karolara tek tek tiklayin ve hedef dügümlerle
+yakin bir döngü yapin.Daha sonra dügümleri ayarlayabilirsiniz.
+Tur algilama ve zamanlamanin dogru çalismasini saglamak 
+icin bitis çizgisi karosundan sonraki ilk karodan baslayin.
+Bitirmek için ilk karoya tekrar tiklayin.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="583"/>
+        <source>Set route: not a valid track.</source>
+        <translation>Rota ayarla: geçerli bir parkur degil.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="584"/>
+        <source>Set route</source>
+        <translation>Rota ayarla</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="584"/>
+        <source>Invalid track. Route cannot be set.</source>
+        <translation>Geçersiz parkur. Rota ayarlanamaz.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="610"/>
+        <source>Set route: route finished.</source>
+        <translation>Rota ayarla: rota bitti.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="637"/>
+        <source>A new track &apos;%1&apos; created. Columns: %2, Rows: %3.</source>
+        <translation>Yeni bir &apos;%1&apos; parkuru olusturuldu. Sütunlar:%2, Satirlar:%3.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="658"/>
+        <location filename="../mainwindow.cpp" line="688"/>
+        <source>Track &apos;</source>
+        <translation>Parkur &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="658"/>
+        <location filename="../mainwindow.cpp" line="688"/>
+        <source>&apos; saved.</source>
+        <translation>&apos;kaydedildi.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="664"/>
+        <source>Failed to save track &apos;</source>
+        <translation>Parkur kaydedilemedi &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="676"/>
+        <source>Save a track</source>
+        <translation>Parkur kaydet</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="694"/>
+        <source>Failed to save track as &apos;</source>
+        <translation>Parça su olarak kaydedilemedi; &apos;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="724"/>
+        <source>Track properties updated.</source>
+        <translation>Parkur özellikleri güncellendi.</translation>
+    </message>
+</context>
+<context>
+    <name>NewTrackDialog</name>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="29"/>
+        <source>Ok</source>
+        <translation>Tamam</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="30"/>
+        <source>Cancel</source>
+        <translation>Iptal</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="34"/>
+        <source>Number of tile columns:</source>
+        <translation>Karo sütunlarinin sayisi:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="35"/>
+        <source>Number of tile rows:</source>
+        <translation>Karo satirlarinin sayisi:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="36"/>
+        <source>Track name:</source>
+        <translation>Parkur adi:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="37"/>
+        <source>User track:</source>
+        <translation>Kullanici parkuru:</translation>
+    </message>
+    <message>
+        <location filename="../newtrackdialog.cpp" line="40"/>
+        <source>Create a new track</source>
+        <translation>Yeni parkur olustur</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../editordata.cpp" line="377"/>
+        <source>Route cleared.</source>
+        <translation>Rota temizlendi.</translation>
+    </message>
+</context>
+<context>
+    <name>QWidget</name>
+    <message>
+        <location filename="../editorview.cpp" line="95"/>
+        <location filename="../editorview.cpp" line="107"/>
+        <source>Rotate 90</source>
+        <translation>Döndür 90</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="95"/>
+        <source> CW..</source>
+        <translation> CW..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="107"/>
+        <source> CCW..</source>
+        <translation> CCw..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="119"/>
+        <source>Clear computer hint</source>
+        <translation>Bilgisayar ipucusunu temizle</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="126"/>
+        <source>Set computer hint &apos;brake hard&apos;..</source>
+        <translation>Bilgisayar ipucusunu &quot;sert fren&quot; ayarlayin ..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="133"/>
+        <source>Set computer hint &apos;brake&apos;..</source>
+        <translation>Bilgisayar ipucusunu &quot;fren&quot; ayarlayin ..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="140"/>
+        <source>Exclude from minimap</source>
+        <translation>Mini haritadan çikar</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="151"/>
+        <source>Insert row before..</source>
+        <translation>Öncesine satir ekle..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="160"/>
+        <source>Insert row after..</source>
+        <translation>Sonrasina satir ekle..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="169"/>
+        <source>Delete row..</source>
+        <translation>Satir sil..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="178"/>
+        <source>Insert column before..</source>
+        <translation>Öncesine sütun ekle..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="187"/>
+        <source>Insert column after..</source>
+        <translation>Sonrasina sütun ekle..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="196"/>
+        <source>Delete column..</source>
+        <translation>Sütun sil..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="225"/>
+        <source>Rotate..</source>
+        <translation>Döndür..</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="240"/>
+        <source>Force stationary</source>
+        <translation>Sabite zorla</translation>
+    </message>
+    <message>
+        <location filename="../editorview.cpp" line="259"/>
+        <source>Set size..</source>
+        <translation>Boyutu ayarla..</translation>
+    </message>
+</context>
+<context>
+    <name>RotateDialog</name>
+    <message>
+        <location filename="../rotatedialog.cpp" line="28"/>
+        <source>Ok</source>
+        <translation>Tamam</translation>
+    </message>
+    <message>
+        <location filename="../rotatedialog.cpp" line="29"/>
+        <source>Cancel</source>
+        <translation>Iptal</translation>
+    </message>
+    <message>
+        <location filename="../rotatedialog.cpp" line="31"/>
+        <source>Angle in degrees:</source>
+        <translation>Derece türünden açi:</translation>
+    </message>
+    <message>
+        <location filename="../rotatedialog.cpp" line="33"/>
+        <source>Rotate Object</source>
+        <translation>Nesneyi Döndür</translation>
+    </message>
+</context>
+<context>
+    <name>TargetNodeSizeDlg</name>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="29"/>
+        <source>Ok</source>
+        <translation>Tamam</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="30"/>
+        <source>Cancel</source>
+        <translation>Iptal</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="32"/>
+        <source>Width:</source>
+        <translation>Genislik:</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="34"/>
+        <source>Height:</source>
+        <translation>Yükseklik:</translation>
+    </message>
+    <message>
+        <location filename="../targetnodesizedlg.cpp" line="36"/>
+        <source>Set target node size</source>
+        <translation>Hedef dügümün boyutunu ayarla</translation>
+    </message>
+</context>
+<context>
+    <name>TrackPropertiesDialog</name>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="30"/>
+        <source>Ok</source>
+        <translation>Tamam</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="31"/>
+        <source>Cancel</source>
+        <translation>Iptal</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="33"/>
+        <source>Name:</source>
+        <translation>Ad:</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="35"/>
+        <source>Index:</source>
+        <translation>Indis:</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="37"/>
+        <source>User track:</source>
+        <translation>Kullanici-parkuru:</translation>
+    </message>
+    <message>
+        <location filename="../trackpropertiesdialog.cpp" line="39"/>
+        <source>Set track properties</source>
+        <translation>Parkur özelliklerini ayarla</translation>
+    </message>
+</context>
+</TS>
