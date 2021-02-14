1ac148d9551b14f3ab8b6ceebb1dc95973a6dd8b
Samet 732 <samethub732@gmail.com>
2021-02-15 02:55:09 +0300

    added Turkish game translations

---
+++ src/game/translations/dustrac-game_tr.ts
@@ -0,0 +1,542 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="../menu/mainmenu.cpp" line="57"/>
+        <source>Play</source>
+        <translation>Oyna</translation>
+    </message>
+    <message>
+        <location filename="../menu/mainmenu.cpp" line="61"/>
+        <source>Help</source>
+        <translation>Yardim</translation>
+    </message>
+    <message>
+        <location filename="../menu/mainmenu.cpp" line="65"/>
+        <source>Credits</source>
+        <translation>Emegi Geçenler</translation>
+    </message>
+    <message>
+        <location filename="../menu/mainmenu.cpp" line="69"/>
+        <source>Quit</source>
+        <translation>Çikis</translation>
+    </message>
+    <message>
+        <location filename="../menu/mainmenu.cpp" line="77"/>
+        <source>Settings</source>
+        <translation>Ayarlar</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../race.cpp" line="70"/>
+        <source>New lap record!</source>
+        <translation>Yeni tur rekoru!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="77"/>
+        <source>New race record!</source>
+        <translation>Yeni yaris rekoru!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="350"/>
+        <source>The Time Trial has ended!</source>
+        <translation>Zaman Testi bitti!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="354"/>
+        <source>The winner has finished!</source>
+        <translation>Birinci bitirdi!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="379"/>
+        <source>Pit stop!</source>
+        <translation>Pit stop!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="572"/>
+        <source>A new best pos!</source>
+        <translation>Yeni en iyi pozisyon!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="582"/>
+        <source>A new track unlocked!</source>
+        <translation>Yeni bir parkurun kilidi açildi!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="586"/>
+        <source>Better luck next time..</source>
+        <translation>Bir dahaki sefere bol sans..</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="629"/>
+        <source>You must stay on track!</source>
+        <translation>Pistde kalmalisin!</translation>
+    </message>
+    <message>
+        <location filename="../race.cpp" line="633"/>
+        <source>Watch your tires!</source>
+        <translation>Lastiklerine dikkat et!</translation>
+    </message>
+    <message>
+        <location filename="../startlights.cpp" line="135"/>
+        <source>GO!!!</source>
+        <translation>GIT!!!</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="52"/>
+        <source>---</source>
+        <translation>---</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="53"/>
+        <source>1st</source>
+        <translation>1.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="54"/>
+        <source>2nd</source>
+        <translation>2.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="55"/>
+        <source>3rd</source>
+        <translation>3.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="56"/>
+        <source>4th</source>
+        <translation>4.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="57"/>
+        <source>5th</source>
+        <translation>5.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="58"/>
+        <source>6th</source>
+        <translation>6.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="59"/>
+        <source>7th</source>
+        <translation>7.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="60"/>
+        <source>8th</source>
+        <translation>8.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="61"/>
+        <source>9th</source>
+        <translation>9.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="62"/>
+        <source>10th</source>
+        <translation>10.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="63"/>
+        <source>11th</source>
+        <translation>11.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="64"/>
+        <source>12th</source>
+        <translation>12.</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="185"/>
+        <source> LAP:</source>
+        <translation> TUR:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="202"/>
+        <source> POS:</source>
+        <extracomment>Position/rank</extracomment>
+        <translation> KONUM:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="210"/>
+        <source>LAP</source>
+        <translation type="unfinished">TUR</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="210"/>
+        <source>LAPS</source>
+        <translation>TURLAR</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="247"/>
+        <source> KM/H</source>
+        <translation> KM/H</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="260"/>
+        <source>LAP:</source>
+        <translation>TUR:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="306"/>
+        <source>L:</source>
+        <extracomment>Last lap time</extracomment>
+        <translatorcomment>Son tur süresi</translatorcomment>
+        <translation>T:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="326"/>
+        <source>R:</source>
+        <extracomment>Lap record time</extracomment>
+        <translatorcomment>Tur rekor süresi</translatorcomment>
+        <translation>R:</translation>
+    </message>
+    <message>
+        <location filename="../timingoverlay.cpp" line="347"/>
+        <source>TOT:</source>
+        <extracomment>Total race time</extracomment>
+        <translatorcomment>Toplam yaris süresi</translatorcomment>
+        <translation>TOPLAM:</translation>
+    </message>
+    <message>
+        <location filename="../menu/confirmationmenu.cpp" line="31"/>
+        <source>Ok</source>
+        <translation>Tamam</translation>
+    </message>
+    <message>
+        <location filename="../menu/confirmationmenu.cpp" line="32"/>
+        <source>Cancel</source>
+        <translation>Iptal</translation>
+    </message>
+    <message>
+        <location filename="../menu/credits.cpp" line="40"/>
+        <source>PROGRAMMING</source>
+        <translation>PROGRAMLAMA</translation>
+    </message>
+    <message>
+        <location filename="../menu/credits.cpp" line="42"/>
+        <source>GRAPHICS</source>
+        <translation>GRAFIKLER</translation>
+    </message>
+    <message>
+        <location filename="../menu/credits.cpp" line="44"/>
+        <source>LEVEL DESIGN</source>
+        <translation>SEVIYE TASARIM</translation>
+    </message>
+    <message>
+        <location filename="../menu/credits.cpp" line="46"/>
+        <source>TRANSLATIONS</source>
+        <translation>TERCÜMELER</translation>
+    </message>
+    <message>
+        <location filename="../menu/credits.cpp" line="48"/>
+        <source>PATCHES</source>
+        <translation>YAMALAR</translation>
+    </message>
+    <message>
+        <location filename="../menu/credits.cpp" line="50"/>
+        <source>SPECIAL THANKS</source>
+        <translation>ÖZEL TESEKKÜR</translation>
+    </message>
+    <message>
+        <location filename="../menu/difficultymenu.cpp" line="42"/>
+        <source>Easy</source>
+        <translation>Kolay</translation>
+    </message>
+    <message>
+        <location filename="../menu/difficultymenu.cpp" line="52"/>
+        <source>Medium</source>
+        <translation>Orta</translation>
+    </message>
+    <message>
+        <location filename="../menu/difficultymenu.cpp" line="62"/>
+        <source>Hard</source>
+        <translation>Zor</translation>
+    </message>
+    <message>
+        <location filename="../menu/help.cpp" line="31"/>
+        <source>GAME GOAL
+
+You are racing against eleven
+computer players.
+
+Your best position will be
+the next start position.
+
+Finish in TOP-6 to unlock
+a new race track!
+
+CONTROLS FOR PLAYER 1
+
+Turn left  : Left
+Turn right : Right
+Accelerate : Up / RIGHT SHIFT
+Brake      : Down / RIGHT CTRL
+
+CONTROLS FOR PLAYER 2
+
+Turn left  : A
+Turn right : D
+Accelerate : W / LEFT SHIFT
+Brake      : S / LEFT CTRL
+
+Quit       : ESC/Q
+Pause      : P</source>
+        <translation>OYUN HEDEFI
+On bir bilgisayar oyuncusuna 
+karsi yarisiyorsunuz.
+
+En iyi konumun bir sonraki
+baslangiç konumun olacak.
+
+Yeni bir yaris parkurunun
+kilidini açmak için ILK-6&apos;da bitir.
+
+OYUNCU 1 IÇIN KONTROLLER
+
+Sola dön : Sol
+Saga dön : Sag
+Hizlan   : Yukari / SAG SHIFT
+Fren     : Asagi / SAG CTRL
+
+OYUNCU 2 IÇIN KONTROLLER
+
+Sola dön : A
+Saga dön : D
+Hizlan   : W / SOL-SHIFT
+Fren     : S / SOL-CTRL
+
+ÇIKIS       : ESC/Q
+Duraklat      : P</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="49"/>
+        <source>Press a key..</source>
+        <translation>Bir tusa basin..</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="89"/>
+        <source>Player One Accelerate</source>
+        <translation>Oyuncu Bir Hizlan</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="98"/>
+        <source>Player One Brake</source>
+        <translation>Oyuncu Bir Fren</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="107"/>
+        <source>Player One Turn Left</source>
+        <translation>Oyuncu Bir Sola Dön</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="116"/>
+        <source>Player One Turn Right</source>
+        <translation>Oyuncu Bir Saga Dön</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="138"/>
+        <source>Player Two Accelerate</source>
+        <translation>Oyuncu Iki Hizlan</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="147"/>
+        <source>Player Two Brake</source>
+        <translation>Oyuncu Iki Fren</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="156"/>
+        <source>Player Two Turn Left</source>
+        <translation>Oyuncu Iki Sola Dön</translation>
+    </message>
+    <message>
+        <location filename="../menu/keyconfigmenu.cpp" line="165"/>
+        <source>Player Two Turn Right</source>
+        <translation>Oyuncu Iki Saga Dön</translation>
+    </message>
+    <message>
+        <location filename="../menu/lapcountmenu.cpp" line="84"/>
+        <source>Choose lap count</source>
+        <translation>Tur sayisini seçiniz</translation>
+    </message>
+    <message>
+        <location filename="../menu/resolutionmenu.cpp" line="115"/>
+        <source>Restart to change the resolution.</source>
+        <translation>Çözünürlügü degistirmek için yeniden baslatin.</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="79"/>
+        <source>Reset best positions?</source>
+        <translation>En iyi konumlar sifirlansin mi?</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="90"/>
+        <source>Reset record times?</source>
+        <translation>Rekor süreleri sifirlansin mi?</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="102"/>
+        <source>Reset unlocked tracks?</source>
+        <translation>Kilidi açilmis parkurlar sifirlansin mi?</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="201"/>
+        <source>Game mode &gt;</source>
+        <translation>Oyun modu &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="205"/>
+        <source>Sounds &gt;</source>
+        <translation>Sesler &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="209"/>
+        <source>GFX &gt;</source>
+        <translation>GFX &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="213"/>
+        <source>Controls &gt;</source>
+        <translation>Kontroller &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="217"/>
+        <source>Reset &gt;</source>
+        <translation>Sifirla &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="237"/>
+        <source>30 fps</source>
+        <translation>30 fps</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="247"/>
+        <source>60 fps</source>
+        <translation>60 fps</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="278"/>
+        <source>Two player race</source>
+        <translation>Çift oyunculu yaris</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="287"/>
+        <source>One player race</source>
+        <translation>Tek oyunculu yaris</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="296"/>
+        <source>Time Trial</source>
+        <translation>Süre testi</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="305"/>
+        <source>Duel</source>
+        <translation>Düello</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="329"/>
+        <source>Vertical</source>
+        <translation>Dikey</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="338"/>
+        <source>Horizontal</source>
+        <translation>Yatay</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="359"/>
+        <source>Full screen resolution &gt;</source>
+        <translation>Tam ekran çözünürlügü &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="363"/>
+        <source>Windowed resolution &gt;</source>
+        <translation>Pencereli çözünürlük &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="367"/>
+        <source>FPS &gt;</source>
+        <translation>FPS &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="371"/>
+        <source>Split type &gt;</source>
+        <translation>Bölünme türü &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="375"/>
+        <source>VSync &gt;</source>
+        <translation>VSync &gt;</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="394"/>
+        <location filename="../menu/vsyncmenu.cpp" line="99"/>
+        <source>Off</source>
+        <translation>Kapat</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="406"/>
+        <location filename="../menu/vsyncmenu.cpp" line="103"/>
+        <source>On</source>
+        <translation>Aç</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="436"/>
+        <source>Reset record times</source>
+        <translation>Rekor sürelerini sifirla</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="440"/>
+        <source>Reset best positions</source>
+        <translation>En iyi konumlari sifirla</translation>
+    </message>
+    <message>
+        <location filename="../menu/settingsmenu.cpp" line="444"/>
+        <source>Reset unlocked tracks</source>
+        <translation>Kilidi açilmis parkurlari sifirla</translation>
+    </message>
+    <message>
+        <location filename="../menu/trackselectionmenu.cpp" line="292"/>
+        <source>       Laps: </source>
+        <translation>       Turlar: </translation>
+    </message>
+    <message>
+        <location filename="../menu/trackselectionmenu.cpp" line="299"/>
+        <source>     Length: </source>
+        <translation>     Uzunluk: </translation>
+    </message>
+    <message>
+        <location filename="../menu/trackselectionmenu.cpp" line="306"/>
+        <source> Lap Record: </source>
+        <translation> Tur Rekoru: </translation>
+    </message>
+    <message>
+        <location filename="../menu/trackselectionmenu.cpp" line="313"/>
+        <source>Race Record: </source>
+        <translation>Yaris Rekoru: </translation>
+    </message>
+    <message>
+        <location filename="../menu/trackselectionmenu.cpp" line="337"/>
+        <source>Finish previous track in TOP-6 to unlock!</source>
+        <extracomment>Try to keep the translation as short as possible.</extracomment>
+        <translation>Kilidi açmak için önceki parkuru ILK-6&apos;da bitirin!</translation>
+    </message>
+    <message>
+        <location filename="../menu/trackselectionmenu.cpp" line="342"/>
+        <source>Unlock it in one/two player race!</source>
+        <extracomment>&quot;it&quot; = a locked track. Try to keep the translation as short as possible.</extracomment>
+        <translation>Tek / çift yarista kilitli bir parkuru açin!</translation>
+    </message>
+    <message>
+        <location filename="../menu/vsyncmenu.cpp" line="75"/>
+        <source>Restart to change VSync setting.</source>
+        <translation>VSync ayarini degistirmek için yeniden baslatin.</translation>
+    </message>
+</context>
+</TS>
